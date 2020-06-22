ba5745ce0917ed1bd4b1958b37e12968427c4d4c
rishabh kalra <rishabh.kalra@niit.com>
2020-06-22 18:02:28 +0530

    updated docker prod script

--- dockbuild-prod.sh
+++ dockbuild-prod.sh
@@ -8,8 +8,8 @@ docker rm web-services.build
 docker rmi -f web-services.build
 unzip dist.zip
 
-docker build -f Dockerfile -t eagle-docker.tarento.com/ui-proxies:gold .
-docker push eagle-docker.tarento.com/ui-proxies:gold
+docker build -f Dockerfile -t 708570229439.dkr.ecr.us-east-1.amazonaws.com/web-services:development-1.0.1 .
+docker push 708570229439.dkr.ecr.us-east-1.amazonaws.com/web-services:development-1.0.1
 
 rm -rf dist
 rm -rf dist.zip
